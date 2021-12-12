31ce7c47d68de19b7cfaf4f4de2cb52bb0d81c48
bernardopiane <bernardopiane@users.noreply.github.com>
2021-12-12 13:41:59 -0300

    Added views, routes, css

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const connectDB = require('./config/db');
+const express = require("express");
+const dotenv = require("dotenv");
+const monrgan = require("morgan");
+const exphbs = require("express-handlebars");
+const connectDB = require("./config/db");
 
 // Load config
-dotenv.config({ path: './config/config.env' });
+dotenv.config({ path: "./config/config.env" });
 
 // Connect to database
 connectDB();
 
 const app = express();
 
+if (process.env.NODE_ENV === "development") {
+  app.use(monrgan("dev"));
+}
+
+// Handlebars Middleware using .hbs extension
+app.engine(".hbs", exphbs.engine({ extname: ".hbs", defaultLayout: "main" }));
+app.set("view engine", ".hbs");
+
+// Define static folder
+app.use(express.static(`${__dirname}/public`));
+
+// Routes
+app.use("/", require("./routes/index"));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`);
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`
+  );
 });
