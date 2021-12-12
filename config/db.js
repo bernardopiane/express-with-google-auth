65c3c38d08935ddbab23fd6f17f47b75438c006f
bernardopiane <bernardopiane@users.noreply.github.com>
2021-12-12 13:23:27 -0300

    db connection

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    });
+
+    console.log(
+      `MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold
+    );
+  } catch (err) {
+    console.error(`Error: ${err.message}`.red.bold);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
