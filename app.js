baf1ce0acd6ffd32960087d4752c7dbcb56d2850
bernardopiane <bernardopiane@users.noreply.github.com>
2021-12-12 13:25:13 -0300

    find&modify depreciated

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 
 // Load config
 dotenv.config({ path: './config/config.env' });
+
+// Connect to database
+connectDB();
 
 const app = express();
 
--- config/db.js
+++ config/db.js
@@ -4,15 +4,12 @@ const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
-      useFindAndModify: false,
       useUnifiedTopology: true,
     });
 
-    console.log(
-      `MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold
-    );
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
-    console.error(`Error: ${err.message}`.red.bold);
+    console.error(`Error: ${err.message}`);
     process.exit(1);
   }
 };
