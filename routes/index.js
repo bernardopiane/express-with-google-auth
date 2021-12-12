21c4e6f137f1d042ff8ce100abb0c851a0f7480f
bernardopiane <bernardopiane@users.noreply.github.com>
2021-12-12 14:55:58 -0300

    Stories display

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
+const Story = require("../models/Story");
+
 // @desc Login/Lading Page
 // @route GET /
 router.get("/", ensureGuest, (req, res) => {
@@ -12,10 +14,19 @@ router.get("/", ensureGuest, (req, res) => {
 
 // @desc Dashboard
 // @route GET /dashboard
-router.get("/dashboard", ensureAuth, (req, res) => {
-  res.render("dashboard", {
-    name: req.user.firstName,
-  });
+router.get("/dashboard", ensureAuth, async (req, res) => {
+  // Fetch user stories
+  try {
+    // Needs .lean() to pass data to handlebars
+    const stories = await Story.find({ user: req.user.id }).lean();
+    res.render("dashboard", {
+      name: req.user.firstName,
+      stories,
+    });
+  } catch (err) {
+    console.error(err);
+    res.render("error/500");
+  }
 });
 
 module.exports = router;
