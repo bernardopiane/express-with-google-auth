798b8df116d241b551f8830a0787f5a86508d179
bernardopiane <bernardopiane@users.noreply.github.com>
2021-12-13 12:33:13 -0300

    Changed port for heroku host

--- app.js
+++ app.js
@@ -91,7 +91,7 @@ app.use("/", require("./routes/index"));
 app.use("/auth", require("./routes/auth"));
 app.use("/stories", require("./routes/stories"));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 80;
 
 app.listen(PORT, () => {
   console.log(
